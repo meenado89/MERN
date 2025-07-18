71f919f315680b93490b984b6d5c932f28f1dc4f
meenado89 <meenakshiemail89@gmail.com>
2025-07-18 15:12:29 +0530

    Adds footer component and improves grid responsiveness
    Integrates a footer component into the main application layout.
    Enhances grid responsiveness in the home component's CSS for better adaptability.
    Fixes missing CSS import in the services component.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import About from "./components/About";
 import Home from "./components/Home";
 import Services from "./components/Services";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
       </Routes>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Home.css";
+//For Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Link} from 'react-router-dom'
+
+const Footer = () => {
+  return (
+    <div >
+     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis, fugiat. Optio libero ab 
+     ipsam veniam qui nobis quo, reiciendis quidem earum autem ea voluptas fugiat praesentium architecto explicabo maiores cupiditate.
+    </div>
+  )
+}
+export default Footer;
