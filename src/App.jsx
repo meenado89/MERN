cfb4120de49f0cfae94f8516f69db810c7716e74
meenado89 <meenakshiemail89@gmail.com>
2025-07-18 14:36:18 +0530

    Adds services section with grid layout and hover effects
    Implements a new services section with a grid layout for displaying service cards. Adds hover animations to enhance interactivity and updates styling in the CSS file. Reintroduces the `Services` component with structured content and placeholder icons for service cards.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,3 @@
-// import About from "./components/About";
-// import Home from "./components/Home";
-// import Services from "./components/Services";
-// import Navbar from './components/NavBar';
-// import {Routes,Route} from 'react-router-dom'
-// function App() {
-//   return (
-//     <>
-//       <Routes>
-//         <Navbar/>
-//         <Route path="/" element={<Home/>}/>
-//         <Route path="/about" element={<About/>}/>
-//         <Route path="/services" element={<Services/>}/>
-//         {/* <Footer/> */}
-//       </Routes>
-//     </>
-//   );
-// }
-
-// export default App;
-
 import About from "./components/About";
 import Home from "./components/Home";
 import Services from "./components/Services";
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,9 +1,61 @@
 
 const Services = () => {
   return (
-    <div>
-      <h1>Services Page</h1>
+    <div >
+      <div className="servicemain-container">
+      <div className="service-header">
+        <h1><b>Services</b></h1>
       <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+      </div>
+      <div className="grid-wrapper">
+        <div className="grid-container">
+          {/* CARD 1 */}
+          <div className="service-card">
+            <div className="main-icon">add icon</div>
+            <div className="sevice-heading">
+              <h4><b>Lorem Ipsum</b></h4>
+            </div>
+            <div className="service-text">
+              <p>Voluptatum deleniti atque corrupti
+              quos dolores et quas molestias</p>
+            </div>
+          </div>
+          {/* CARD 2 */}
+          <div className="service-card">
+            <div className="main-icon">add icon</div>
+            <div className="sevice-heading">
+              <h4><b>Lorem Ipsum</b></h4>
+            </div>
+            <div className="service-text">
+              <p>Voluptatum deleniti atque corrupti
+              quos dolores et quas molestias</p>
+            </div>
+          </div>
+          {/* CARD 3 */}
+          <div className="service-card">
+            <div className="main-icon">add icon</div>
+            <div className="sevice-heading">
+              <h4><b>Lorem Ipsum</b></h4>
+            </div>
+            <div className="service-text">
+              <p>Voluptatum deleniti atque corrupti
+              quos dolores et quas molestias</p>
+            </div>
+          </div>
+          {/* CARD 4 */}
+          <div className="service-card">
+            <div className="main-icon">add icon</div>
+            <div className="sevice-heading">
+              <h4><b>Lorem Ipsum</b></h4>
+            </div>
+            <div className="service-text">
+              <p>Voluptatum deleniti atque corrupti
+              quos dolores et quas molestias</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
     </div>
   );
 };
