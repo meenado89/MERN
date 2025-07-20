303bd99be6b9b8433b58f91cf2f58a06b89bc8cf
meenado89 <meenakshiemail89@gmail.com>
2025-07-20 16:37:13 +0530

    Enhances footer layout and updates CSS styling
    Refactors the footer component with improved structure, adding
    sections for contact info, useful links, services, and social
    media. Introduces responsive grid-based styling for the footer
    and refines existing CSS by improving formatting, consistency,
    and hover effects. Adds new styles for the top footer layout
    and updates service card interactions for a better user
    experience.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,62 @@ import {Link} from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div >
-     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis, fugiat. Optio libero ab 
-     ipsam veniam qui nobis quo, reiciendis quidem earum autem ea voluptas fugiat praesentium architecto explicabo maiores cupiditate.
+    <>
+    <div className="container-fluid" >
+    <section className="top-footer">
+      {/* section 1 */}
+      <div className="footer-container">
+        <ul>
+          <h1>ARSHA</h1>
+            <li>A108 Adam Street</li>
+            <li>New York, NY 535022</li>
+            <li><b>Phone:</b>
+            +1 5589 55488 55
+            </li>
+            <li>
+              <b>Email:</b>
+              info@example.com
+            </li>
+        </ul>
+      </div>
+      {/* section 2 */}
+      <div className="footer-container">
+        <ul>
+          <h3>Usefull Links</h3>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Services</li>
+          <li>Terms of service</li>
+        </ul>
+      </div>
+
+
+      {/* SECTION 3 */}
+      <div className="footer-container">
+          <ul>
+          <h3>Our Services</h3>
+          <li>Web Design</li>
+          <li>Web Development</li>
+          <li>Product Management</li>
+          <li>Marketing</li>
+        </ul>
+      </div>
+
+
+      {/* SECTION 4 */}
+      <div className="footer-container">
+      <h3>Follow Us</h3>
+      <br />
+      <p>Cras fermentum odio eu feugiat lide par naso tierra
+        videa magna derita valies
+      </p>
+      <div className="icons">
+        Add Icons 
+      </div>
+      </div>
+    </section>
     </div>
+    </>
   )
 }
 export default Footer;
